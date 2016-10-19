dac50a2b7db80c830e03290f68cef0f5fe5fce59
gTopa <gtopa@stuy.edu>
2016-10-19 15:11:18 -0400

    notes

---
+++ 1019/doNow.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  printf("%d\n", 1>>2);
+  printf("%d\n", 1<<2);
+  printf("%d\n", 1<<4);
+  printf("%d\n", 1>>4);
+  printf("%d\n", 1>>3);
+  printf("%d\n", 1<<3);
+  printf("%d\n", 2<<4);
+  printf("%d\n", 2>>4);
+  return 0;
+}
